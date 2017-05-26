78fc51f2241af32eea39ffc895eb820e8e47faba
Matheus Lessa Rodrigues <matheuslessarodrigues@gmail.com>
2017-05-26 12:21:06 -0300

    Update Player.cs

--- MonoGame/Player.cs
+++ MonoGame/Player.cs
@@ -1,44 +1,99 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace MonoGame
 {
 	public class Player
 	{
-		public Vector2 position = new Vector2( 0.0f, 0.0f );
+		public Vector2 position = new Vector2( 0.0f, 100.0f );
 		public Vector2 velocity = new Vector2( 100.0f, 100.0f );
 
-		public Texture2D texture;
-		public Color color = new Color( 1.0f, 1.0f, 1.0f, 1.0f );
+		public Texture2D textureParado;
+        public Texture2D[] texturesAndando;
+        public Color color = new Color( 1.0f, 1.0f, 1.0f, 1.0f );
 
-		public bool deveDesenhar = true;
+		public int indiceAnimacao = 0;
 		public double contadorTempo = 0.0;
+
+        public bool olhandoDireita;
+        public bool estaAndando;
 
 		public void Load( ContentManager content )
 		{
-			texture = content.Load<Texture2D>( "PokemonSprite" );
-		}
+			textureParado = content.Load<Texture2D>( "megaman1" );
+
+            texturesAndando = new Texture2D[4];
+            texturesAndando[0] = content.Load<Texture2D>("megaman3");
+            texturesAndando[1] = content.Load<Texture2D>("megaman2");
+            texturesAndando[2] = content.Load<Texture2D>("megaman3");
+            texturesAndando[3] = content.Load<Texture2D>("megaman4");
+        }
+
+        public void TentaAtirar( Tiro tiro )
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                tiro.position = position;
+
+                if( olhandoDireita )
+                    tiro.velocity = new Vector2(1000.0f, 0.0f);
+                else
+                    tiro.velocity = new Vector2(-1000.0f, 0.0f);
+            }
+        }
 
 		public void Update( GameTime gameTime )
 		{
-			contadorTempo += gameTime.ElapsedGameTime.TotalMilliseconds;
-			if( contadorTempo > 100.0 )
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                position.X += 4.0f;
+                olhandoDireita = true;
+                estaAndando = true;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                position.X -= 4.0f;
+                olhandoDireita = false;
+                estaAndando = true;
+            }
+            else
+            {
+                estaAndando = false;
+            }
+
+            contadorTempo += gameTime.ElapsedGameTime.TotalMilliseconds;
+			if( contadorTempo > 300.0 )
 			{
-				if( deveDesenhar )
-					deveDesenhar = false;
-				else
-					deveDesenhar = true;
+                indiceAnimacao += 1;
+				if( indiceAnimacao == texturesAndando.Length )
+					indiceAnimacao = 0;
+
 				contadorTempo = 0.0;
 			}
 		}
 
 		public void Draw( SpriteBatch spriteBatch )
 		{
-			if( deveDesenhar )
-			{
-				spriteBatch.Draw( texture, position, color );
-			}
-		}
+            SpriteEffects flip;
+            if (olhandoDireita)
+                flip = SpriteEffects.FlipHorizontally;
+            else
+                flip = SpriteEffects.None;
+
+            Texture2D texture;
+            if (estaAndando)
+            {
+                texture = texturesAndando[indiceAnimacao];
+            }
+            else
+            {
+                texture = textureParado;
+            }
+
+            spriteBatch.Draw(texture, position, null, color, 0.0f,
+                Vector2.Zero, 5.0f, flip, 0.0f);
+        }
 	}
 }
